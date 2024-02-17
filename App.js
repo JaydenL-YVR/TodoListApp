19ff0e387e9476672729e107fa758d8033c4d981
Jayden <jayden.liwanag@edu.sait.ca>
2024-02-17 14:26:47 -0700

    Final Commit

--- App.js
+++ App.js
@@ -8,14 +8,14 @@
 
 import React from 'react';
 import {
-    SafeAreaView,
-    StyleSheet,
-    Pressable,
-    View,
-    Text,
-    ScrollView,
-    TextInput,
-    Button
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button
 } from 'react-native';
 
 import ToDoList from './components/ToDoList';
@@ -23,7 +23,7 @@ import ToDoForm from './components/ToDoForm';
 
 function App() {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <ToDoList style={styles.todolist} />
             <ToDoForm style={styles.todoform} />
         </SafeAreaView>
@@ -31,12 +31,17 @@ function App() {
 }
 
 const styles = StyleSheet.create({
-    todolist: {
-      
+    container: {
+        flex: 1,
+
     },
-    
+
+    todolist: {
+
+    },
+
     todoform: {
-        
+
     },
 });
 
